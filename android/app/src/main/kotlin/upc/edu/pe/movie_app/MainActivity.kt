c68d41f300b77c2891380f07064bc44623b682d2
Angel Cancho <u201721995@upc.edu.pe>
2024-11-26 21:02:01 -0500

    chore: project initial setup

---
+++ android/app/src/main/kotlin/upc/edu/pe/movie_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package upc.edu.pe.movie_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/movies/domain/movie.dart
@@ -0,0 +1,16 @@
+class Movie {
+  final int id;
+  final String title;
+  final String overview;
+  final String releaseDate;
+  final String posterPath;
+  final double voteAverage;
+
+  Movie(
+      {required this.id,
+      required this.title,
+      required this.overview,
+      required this.releaseDate,
+      required this.posterPath,
+      required this.voteAverage});
+}
