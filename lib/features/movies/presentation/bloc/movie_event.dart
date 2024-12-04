5b07c18a382387338f588cb0d6051fe6a21bed95
Angel Cancho <u201721995@upc.edu.pe>
2024-12-03 20:46:55 -0500

    feat(core): implement BLoC for state management

---
+++ lib/features/movies/presentation/bloc/movie_event.dart
@@ -0,0 +1,8 @@
+abstract class MovieEvent {}
+
+class GetMoviesEvent extends MovieEvent {
+  final String endpoint;
+  final int page;
+
+  GetMoviesEvent({required this.endpoint, required this.page});
+}
